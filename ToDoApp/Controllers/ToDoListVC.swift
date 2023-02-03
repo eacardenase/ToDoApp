5b31bfff2e996c638679bd356cdeeb57cb6fe652
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-03 10:52:53 -0500

    Adding new todos to our todo array with a textField and alert controller

--- ToDoApp/Controllers/ToDoListVC.swift
+++ ToDoApp/Controllers/ToDoListVC.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ToDoListVC: UITableViewController {
     
-    let itemsArray = [
+    var itemsArray = [
         "Be an iOS expert",
         "Read Fahrenheit 421",
         "Find happyness"
@@ -18,9 +18,8 @@ class ToDoListVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().barTintColor = .systemCyan
-//        UINavigationBar.appearance().
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addToDo))
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,6 +31,47 @@ class ToDoListVC: UITableViewController {
         cell.textLabel?.text = itemsArray[indexPath.row]
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        } else {
+            cell?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @objc func addToDo() {
+        let ac = UIAlertController(title: "Add ToDo", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            
+            guard let todo = ac?.textFields?[0].text else { return }
+            
+            self?.submit(todo)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ todo: String) {
+        if todo.count < 3 {
+            return
+        }
+        
+        itemsArray.insert(todo, at: 0)
+
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
     }
 
 }
